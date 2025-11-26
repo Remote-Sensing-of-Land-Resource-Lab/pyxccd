3b698d10090c1cedfe6bb223b411edd6dde5f476
SuYe99 <remotesensingsuy@gmail.com>
2025-11-26 20:40:17 +0800

    continue to fix the bug for mac test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -220,9 +220,14 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+if sphinx_rtd_theme is not None:
+    # Normal docs build: RTD theme is installed
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    # Fallback when the theme isn't installed (e.g. during tests)
+    html_theme = "alabaster"
+    html_theme_path = []
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
